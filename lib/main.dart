ee73b81ebd536f0c77ae84ec0e08c9dac27a9bd2
arifhossainrumi <arif.hossain.cse@ulab.edu.bd>
2025-03-18 10:41:35 +0600

    Updated web build

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:dropdown_search/dropdown_search.dart';
+
+import 'function/locations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'House Rent Predictor',
+      title: 'Dhaka House Rent Predictor',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: RentPredictionScreen(),
     );
@@ -29,11 +32,45 @@ class _RentPredictionScreenState extends State<RentPredictionScreen> {
   final TextEditingController areaController = TextEditingController();
   final TextEditingController bedController = TextEditingController();
   final TextEditingController bathController = TextEditingController();
-  final TextEditingController locationController = TextEditingController();
   String? predictedPrice;
+  String? selectedLocation;
+  bool isLoading = false;
+
+
 
   Future<void> predictRent() async {
-    final url = Uri.parse('https://house-rent-prediction-eg9o.onrender.com/predict'); // Update for deployment
+    setState(() {
+      isLoading = true;
+    });
+
+    Future.delayed(const Duration(seconds: 5), () {
+      if (mounted && isLoading) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text(
+              "Taking longer than expected. Please wait...",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            duration: const Duration(seconds: 5),
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: const EdgeInsets.all(16),
+            backgroundColor: Colors.blueAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            action: SnackBarAction(
+              label: "OK",
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        );
+      }
+    });
+
+    final url =
+    Uri.parse('https://house-rent-prediction-eg9o.onrender.com/predict');
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
@@ -41,7 +78,7 @@ class _RentPredictionScreenState extends State<RentPredictionScreen> {
         "Area": int.parse(areaController.text),
         "Bed": int.parse(bedController.text),
         "Bath": int.parse(bathController.text),
-        "Location": locationController.text
+        "Location": selectedLocation
       }),
     );
 
@@ -50,26 +87,71 @@ class _RentPredictionScreenState extends State<RentPredictionScreen> {
       predictedPrice = data['predicted_price'] != null
           ? "Predicted Rent: ${data['predicted_price']} BDT"
           : "Error: ${data['error']}";
+      isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('House Rent Predictor')),
+      appBar: AppBar(title: Text('Dhaka House Rent Predictor')),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextField(controller: areaController, decoration: InputDecoration(labelText: 'Area (sqft)'), keyboardType: TextInputType.number),
-            TextField(controller: bedController, decoration: InputDecoration(labelText: 'Number of Beds'), keyboardType: TextInputType.number),
-            TextField(controller: bathController, decoration: InputDecoration(labelText: 'Number of Baths'), keyboardType: TextInputType.number),
-            TextField(controller: locationController, decoration: InputDecoration(labelText: 'Location')),
+            TextField(
+                controller: areaController,
+                decoration: InputDecoration(labelText: 'Area (sqft)'),
+                keyboardType: TextInputType.number),
+            TextField(
+                controller: bedController,
+                decoration: InputDecoration(labelText: 'Number of Beds'),
+                keyboardType: TextInputType.number),
+            TextField(
+                controller: bathController,
+                decoration: InputDecoration(labelText: 'Number of Baths'),
+                keyboardType: TextInputType.number),
+            SizedBox(height: 10),
+            DropdownSearch<String>(
+              popupProps: const PopupProps.menu(
+                title: Text(
+                  'Supported Areas',
+                  textAlign: TextAlign.center,
+                ),
+                fit: FlexFit.loose,
+                showSearchBox: true,
+                showSelectedItems: true,
+                searchFieldProps: TextFieldProps(
+                  decoration: InputDecoration(
+                    hintText: "Type location name...",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              items: locations,
+              onSaved: (value) => selectedLocation = value!,
+              dropdownDecoratorProps: const DropDownDecoratorProps(
+                dropdownSearchDecoration: InputDecoration(
+                  labelText: "Location",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  selectedLocation = value;
+                });
+              },
+              selectedItem: selectedLocation,
+            ),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: predictRent, child: Text('Predict Rent')),
+            isLoading
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                onPressed: predictRent, child: Text('Predict Rent')),
             SizedBox(height: 20),
             if (predictedPrice != null)
-              Text(predictedPrice!, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+              Text(predictedPrice!,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
           ],
         ),
       ),
